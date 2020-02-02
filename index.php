5220be55edc94362191c6cb092b7b713e5058376
hidoyatmz <hidoyatmz@gmail.com>
2020-02-02 12:03:12 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,323 @@
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Hidoyat.fr</title>
+	<meta name="keywords" content="mehdi zaidi mehdizaidi contactmehdizaidi">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="Description" content="Hidoyat.fr est un site de presentation de Mehdi Zaidi, étudiant et passioné par l'Informatique et le développement en général." />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+</head>
+<body>
+	<div class="wrapper">
+		<!-- Sidebar -->
+		<nav id="sidebar">
+			<div class="sidebar-header">
+				<h3>Hidoyatmz.fr</h3>
+			</div>
+			<ul class="list-unstyled components">
+				<img id="avatar" src="img/avatar.png">
+				<li>
+					<a href="#presentation"><i class="fas fa-user"></i></a>
+				</li>
+				<li>
+					<a href="#purplediv"><i class="fas fa-laptop-code"></i></a>
+				</li>
+				<li>
+					<a href="#projets"><i class="fas fa-briefcase"></i></a>
+				</li>
+				<li>
+					<a href="#contact"><i class="fab fa-telegram-plane"></i></a>
+				</li>
+			</ul>
+		</nav>
+	</div>
+	<div class="background"></div>
+	<div id="homepage">
+		<div class="me">
+			<h1><i class="fas fa-cog mycog"></i> Mehdi <a>"Hidoyatmz"</a> Zaidi</h1>
+			<p>Je suis un passioné de développement et de jeux vidéos.<br>Ce site est un portfolio voué à me présenter, personellement ainsi que mes compétences.</p>
+			<i class="fas fa-arrow-down godown"></i>
+		</div>
+	</div>
+	<div class="presentation" id="presentation">
+		<div class="container">
+			<div data-aos="flip-up" data-aos-duration="500">
+				<h1><i class="far fa-question-circle"></i> Qui suis-je</h1>
+				<hr>
+				<p>
+					Bonjour à tous, je m'appelle Mehdi (Hidoyat), j'ai 17 ans et je suis passioné de developpement depuis maintenant 7 ans. Que ce soit sur les jeux vidéos auxquels j'ai joué ou même le web, j'ai toujours aimé voir ce qui se passait derrière toutes ces choses, c'est donc ma curiosité qui m'a amené à developper ! Aujourd'hui au lycée en filière S, j'envisage mes études dans le developpement (web, applications, jeux...) en suivant un DUT Informatique.
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="purplediv" id="purplediv">
+		<div class="container">
+			<h1><i class="fas fa-laptop-code titleicon"></i> Mes compétences</h1>
+			<hr>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="purplediv-text">
+						<div data-aos="zoom-out" data-aos-duration="1000">
+							<p>
+								Durant ces 7 années de passions, j'ai appris certaines choses, je me concentre énormement sur le développement web bien que j'ai de bonne bases en Lua et quelques notions en C++ que j'ai du utiliser dans certains jeux vidéo.<br>Ci-contre ce sont les languages/framework que je maîtrise/utilise le plus. Bien que ça ne soit pas très représentatif, c'est mon auto-évaluation sur les langages concernés.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div data-aos="zoom-out" data-aos-duration="1000">
+						<div class="skillbar clearfix " data-percent="20%">
+							<div class="skillbar-title" style="background: #d35400;"><span>HTML5</span></div>
+							<div id="html" class="skillbar-bar" style="background: #e67e22;width:95%;"></div>
+							<div class="skill-bar-percent">95%</div>
+						</div> <!-- End Skill Bar -->
+					</div>
+
+					<div data-aos="zoom-out" data-aos-duration="1100">
+						<div class="skillbar clearfix " data-percent="25%">
+							<div class="skillbar-title" style="background: #2980b9;"><span>CSS3</span></div>
+							<div id="css" class="skillbar-bar" style="background: #3498db;width:80%;"></div>
+							<div class="skill-bar-percent">80%</div>
+						</div> <!-- End Skill Bar -->
+					</div>
+
+					<div data-aos="zoom-out" data-aos-duration="1100">
+						<div class="skillbar clearfix " data-percent="25%">
+							<div class="skillbar-title" style="background: #5c0091;"><span>Bootstrap</span></div>
+							<div id="css" class="skillbar-bar" style="background: #5c0091;width:80%;"></div>
+							<div class="skill-bar-percent">80%</div>
+						</div> <!-- End Skill Bar -->
+					</div>
+
+					<div data-aos="zoom-out" data-aos-duration="1400">
+						<div class="skillbar clearfix " data-percent="80%">
+							<div class="skillbar-title" style="background: #27ae60;"><span>MySQL</span></div>
+							<div id="mysql" class="skillbar-bar" style="background: #2ecc71;width:80%;"></div>
+							<div class="skill-bar-percent">80%</div>
+						</div> <!-- End Skill Bar -->
+					</div>
+
+					<div data-aos="zoom-out" data-aos-duration="1300">
+						<div class="skillbar clearfix " data-percent="70%">
+							<div class="skillbar-title" style="background: #46465e;"><span>PHP</span></div>
+							<div id="php" class="skillbar-bar" style="background: #5a68a5;width:70%;"></div>
+							<div class="skill-bar-percent">70%</div>
+						</div> <!-- End Skill Bar -->
+					</div>
+
+					<div data-aos="zoom-out" data-aos-duration="1200">
+						<div class="skillbar clearfix " data-percent="50%">
+							<div class="skillbar-title" style="background: #2c3e50;"><span>Javascript</span></div>
+							<div id="js" class="skillbar-bar" style="background: #2c3e50;width:50%;"></div>
+							<div class="skill-bar-percent">50%</div>
+						</div> <!-- End Skill Bar -->
+					</div>
+
+					<div data-aos="zoom-out" data-aos-duration="1500">
+						<div class="skillbar clearfix " data-percent="70%">
+							<div class="skillbar-title" style="background: #124e8c;"><span>Laravel</span></div>
+							<div id="laravel" class="skillbar-bar" style="background: #4288d0;width:5%;"></div>
+							<div class="skill-bar-percent">5%</div>
+						</div> <!-- End Skill Bar -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="presentation" id="projets">
+		<div class="container-fluid">
+			<h1><i class="fas fa-briefcase"></i> Mon éxperience</h1>
+			<div class="container"> 
+				<hr>  
+			</div>
+			<div class="avtl">
+				<div data-aos="fade-up" data-aos-duration="1500">
+					<div class="ewrap right">
+						<div class="event">
+							<strong class="date">Année 2019</strong>
+							<p class="text">
+								Découverte du C++, acquisition de solides bases en Lua, débuts dans l'émulation du jeu "world of warcraft", prise en main de debian et apprentissage du javascript.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div data-aos="fade-up" data-aos-duration="1500">
+					<div class="ewrap left">
+						<div class="event">
+							<strong class="date">Année 2018</strong>
+							<p class="text">
+								Poursuite de mon apprentissage dans le développement web (mysql/php) avec un stage de seconde chez <i><b>services nomades</b></i> en janvier et de l'entrainement sur des projets personnels.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div data-aos="fade-up" data-aos-duration="1500">
+					<div class="ewrap right">
+						<div class="event">
+							<strong class="date">Année 2017</strong>
+							<p class="text">
+								Début dans le développement web, apprentissage du html/css et stage de troisième chez <i>services nomades</i> en janvier (une entreprise de développement web et applications ios et android).
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div data-aos="fade-up" data-aos-duration="1500">
+					<div class="ewrap left">
+						<div class="event">
+							<strong class="date">Année 2016</strong>
+							<p class="text">
+								Découverte du développement web à travers différents cms/template, modification de ces derniers et customisation simple du css (couleurs, taille des textes).
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div data-aos="fade-up" data-aos-duration="1500">
+					<div class="ewrap right">
+						<div class="event">
+							<strong class="date">Année 2015</strong>
+							<p class="text">
+								Creation de petits addons sur le jeu "Garry's Mod" en Glua, une "variante" du Lua.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div data-aos="fade-up" data-aos-duration="1500">
+					<div class="ewrap left">
+						<div class="event">
+							<strong class="date">Années 2012-2014</strong>
+							<p class="text">
+								Début du "codage" sur minecraft durant mon enfance, tentative de créer mes propres petits addons en partant de la base de scripts éxistant.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>  
+		</div>
+	</div>
+	<!--
+	Div lié au contact
+	Comprends : mon email, mon numéro de téléphone et mon pseudo discord.
+	Une div 6/12 avec les informations ainsi qu'une div 6/12 avec le widget de mon serveur discord.
+	-->
+	<div class="purplediv" id="contact">
+		<div class="container">
+			<h1><i class="fas fa-laptop-code titleicon"></i> Me contacter</h1>
+			<hr>
+			<div data-aos="fade-right" data-aos-duration="1500">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="purplediv-text contacts">
+							<div class="mailcontact">
+								<span><i class="fas fa-envelope"></i> Email :</span>
+								<p id="email">hidoyatmz@gmail.com <button><i id="emailcopybutton" class="far fa-copy titleicon" onclick="copyemail()"></i></button></p>
+							</div>
+							<div class="phonecontact">
+								<span><i class="fas fa-phone-square"></i> Téléphone :</span>
+								<p>+33761599851 <button><i id="phonecopybutton" class="far fa-copy titleicon" onclick="copyphone()"></i></button></p>
+							</div>
+							<div class="discordcontact">
+								<span><i class="fab fa-discord"></i> Discord :</span>
+								<p>Hidoyatmz#1999 <button><i id="discordcopybutton" class="far fa-copy titleicon" onclick="copydiscord()"></i></button></p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<iframe src="https://discordapp.com/widget?id=582277452014026770&theme=dark" width="100%" height="350" allowtransparency="true" frameborder="0" style="margin-top: -15px;"></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		<!-- 
+		On est clairement pas sur le code javascript le plus opti mais au moins ça fonctionne :D
+		-->
+		<script type="text/javascript">
+			function copyemail(self) {
+				var email = "hidoyatmz@gmail.com";
+				var emailinput = document.createElement("input");
+				  document.body.appendChild(emailinput);
+				  emailinput.setAttribute("id", "email_input");
+				  document.getElementById("email_input").value=email;
+				  emailinput.select();
+				  document.execCommand("copy");
+				  document.body.removeChild(emailinput);
+
+				  var button = document.getElementById("emailcopybutton");
+				  button.classList.remove('fa-copy');
+				  button.classList.add('fa-check-circle');
+
+				  var phonebutton = document.getElementById("phonecopybutton");
+				  phonebutton.classList.remove('fa-copy');
+				  phonebutton.classList.remove('fa-check-circle');
+				  phonebutton.classList.add('fa-copy');
+
+				  var discordbutton = document.getElementById("discordcopybutton");
+				  discordbutton.classList.remove('fa-copy');
+				  discordbutton.classList.remove('fa-check-circle');
+				  discordbutton.classList.add('fa-copy');
+			}
+
+			function copyphone(self) {
+				var phone = "0761599851";
+				var phoneinput = document.createElement("input");
+				  document.body.appendChild(phoneinput);
+				  phoneinput.setAttribute("id", "email_input");
+				  document.getElementById("email_input").value=phone;
+				  phoneinput.select();
+				  document.execCommand("copy");
+				  document.body.removeChild(phoneinput);
+
+				  var button = document.getElementById("emailcopybutton");
+				  button.classList.remove('fa-copy');
+				  button.classList.remove('fa-check-circle');
+				  button.classList.add('fa-copy');
+
+				  var phonebutton = document.getElementById("phonecopybutton");
+				  phonebutton.classList.remove('fa-copy');
+				  phonebutton.classList.add('fa-check-circle');
+
+				  var discordbutton = document.getElementById("discordcopybutton");
+				  discordbutton.classList.remove('fa-copy');
+				  discordbutton.classList.remove('fa-check-circle');
+				  discordbutton.classList.add('fa-copy');
+			}
+
+			function copydiscord(self) {
+				var discord = "Hidoyatmz#1999";
+				var discordinput = document.createElement("input");
+				  document.body.appendChild(discordinput);
+				  discordinput.setAttribute("id", "email_input");
+				  document.getElementById("email_input").value=discord;
+				  discordinput.select();
+				  document.execCommand("copy");
+				  document.body.removeChild(discordinput);
+
+				  var discordbutton = document.getElementById("discordcopybutton");
+				  discordbutton.classList.remove('fa-copy');
+				  discordbutton.classList.add('fa-check-circle');
+
+				  var button = document.getElementById("emailcopybutton");
+				  button.classList.remove('fa-copy');
+				  button.classList.remove('fa-check-circle');
+				  button.classList.add('fa-copy');
+
+				  var phonebutton = document.getElementById("phonecopybutton");
+				  phonebutton.classList.remove('fa-copy');
+				  phonebutton.classList.remove('fa-check-circle');
+				  phonebutton.classList.add('fa-copy');
+			}
+		</script>
+		<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+		<script>
+			AOS.init();
+		</script>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	</body>
